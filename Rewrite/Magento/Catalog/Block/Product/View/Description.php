b4b6385942fcf31c895798d0335406ce2d6b3f36
Miguel Abache <e_meflorez@stefanini.com>
2025-11-19 23:03:58 -0500

    sección 2 - Ejercicio práctico de desarrollo - sobreescribir bloque existente

---
+++ Rewrite/Magento/Catalog/Block/Product/View/Description.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Vendor\HelloWorld\Rewrite\Magento\Catalog\Block\Product\View;
+
+class Description extends \Magento\Catalog\Block\Product\View\Description
+{
+    public function toHtml()
+    {
+        $originalHtml = parent::toHtml();
+        $customText = __('Descripción modificada por Vendor_HelloWorld');
+        
+        return $originalHtml . '<div class="vendor-helloworld-custom">' . 
+               $this->escapeHtml($customText) . '</div>';
+    }
+}
+
